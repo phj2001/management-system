3f45660c33989a4b106ba67b0071c757ed6859be
phj2001 <2183846277@qq.com>
2025-07-16 23:56:43 +0800

    这是一个通过分支进行修改的例子

--- backend/src/main/java/com/example/management_system/HelloController.java
+++ backend/src/main/java/com/example/management_system/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "你好，这是一个来自后端的返回消息！";
+        return "你好，这是一个来自后端的返回消息！然后我添加了这几个字，来表示修改。";
     }
 }
